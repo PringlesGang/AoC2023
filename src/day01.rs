d149c4b1268622fc941f5e586f388e9f5a89873f
PringlesGang <liamelderbroek123@gmail.com>
2025-01-11 00:44:36 +0100

    Implemented 1-2

--- src/day01.rs
+++ src/day01.rs
@@ -6,34 +6,45 @@ fn read_input() -> Result<Vec<String>, &'static str> {
     Ok(document.lines().map(ToString::to_string).collect())
 }
 
-fn get_match(line: &str, productions: &HashMap<String, u32>) -> u32 {
-    let string = match productions.keys()
+fn get_match(line: &str, productions: &HashMap<&str, u32>, reverse: bool) -> u32 {
+    let directed_line = if reverse {
+        line.chars().rev().collect()
+    } else {
+        line.to_string()
+    };
+
+    let string = productions.keys()
         .collect::<Vec<_>>()
         .into_iter()
-        .map(
-            |string|
+        .map(|string| {
+            let directed_string = if reverse {
+                string.chars().rev().collect()
+            } else {
+                (*string).to_string()
+            };
+
             (
                 string,
-                line.find(string as &str)
+                directed_line
+                    .find(&directed_string)
                     .unwrap_or(line.len())
             )
-        )
-        .reduce(|(string1, loc1), (string2, loc2)| if loc1 <= loc2 { (string1, loc1) } else { (string2, loc2) } ) {
-        Some(x) => x.0,
-        None => "0"
-    };
+        })
+        .reduce(|(string1, loc1), (string2, loc2)|
+            if loc1 <= loc2 { (string1, loc1) } else { (string2, loc2) }
+        ).map_or("0", |(string, _)| string);
 
     *productions.get(string).unwrap_or(&0)
 }
 
-fn get_calibration_value(line: &str, productions: &HashMap<String, u32>) -> u32 {
-    let first_num = get_match(line, productions);
-    let last_num = get_match(line.chars().rev().collect::<String>().as_str(), productions);
+fn get_calibration_value(line: &str, productions: &HashMap<&str, u32>) -> u32 {
+    let first_num = get_match(line, productions, false);
+    let last_num = get_match(line, productions, true);
 
     first_num * 10 + last_num
 }
 
-fn get_sum(productions: &HashMap<String, u32>) -> Result<u32, &'static str> {
+fn get_sum(productions: &HashMap<&str, u32>) -> Result<u32, &'static str> {
     let lines = read_input()?;
 
     let sum = lines.iter()
@@ -44,10 +55,49 @@ fn get_sum(productions: &HashMap<String, u32>) -> Result<u32, &'static str> {
 }
 
 pub fn solve_1() {
-    let productions = (0 .. 10).map(|num: u32| (num.to_string(), num)).collect::<HashMap<_, _>>();
+    println!("Day 01 part 1:");
+
+    let num_strings = (0 .. 10).map(|num| num.to_string()).collect::<Vec<_>>();
+
+    #[allow(clippy::cast_possible_truncation)]
+    let productions: HashMap<&str, u32> = num_strings.iter()
+        .enumerate()
+        .map(|(i, string)| (string.as_str(), i as u32))
+        .collect();
 
     match get_sum(&productions) {
         Ok(sum) => println!("The sum of all calibration values is {sum}"),
         Err(msg) => eprintln!("{msg}")
     }
+
+    println!();
+}
+
+pub fn solve_2() {
+    println!("Day 01 part 2:");
+
+    let num_strings = (0 .. 10).map(|num| num.to_string()).collect::<Vec<_>>();
+
+    #[allow(clippy::cast_possible_truncation)]
+    let productions: HashMap<&str, u32> = num_strings.iter()
+        .enumerate()
+        .map(|(i, string)| (string.as_str(), i as u32))
+        .chain([
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9)
+        ]).collect();
+
+    match get_sum(&productions) {
+        Ok(sum) => println!("The sum of all calibration values is {sum}"),
+        Err(msg) => eprintln!("{msg}")
+    }
+
+    println!();
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ mod day01;
 fn main() {
     println!("Solving all 2023 days...\n");
     
-    println!("Day 01:");
     day01::solve_1();
+    day01::solve_2();
 }
